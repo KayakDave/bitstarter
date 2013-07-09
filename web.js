d6670a4ceabf658770071eef841abdb82694c6ac
Ubuntu <ubuntu@ip-172-31-24-178.us-west-2.compute.internal>
2013-07-09 20:27:53 +0000

    Use index.html

--- web.js
+++ web.js
@@ -6,7 +6,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
    fs.readFileSync('./index.html', function(err,data) {
       if (err) throw err;
-   response.send(buf.toString(data));
+response.send("Hello");
+   var buffer = buf.toString(data);
+   response.send(buffer));
 });
 
 var port = process.env.PORT || 5000;
