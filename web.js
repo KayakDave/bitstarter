2951c279aecf9d60238bd79c3ad920941dfb4d36
Ubuntu <ubuntu@ip-172-31-24-178.us-west-2.compute.internal>
2013-07-09 20:56:40 +0000

    Use index.html

--- web.js
+++ web.js
@@ -1,18 +1,23 @@
 var express = require('express'),
-    fs= require('fs');
+    fs= require('fs'),
+    buf=require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 response.send("Hello");
-   fs.readFileSync('./index.html', function(err,data) {
-      if (err) throw err;
+   var data= fs.readFileSync('./index.html');
    var buffer = buf.toString(data);
+   console.log('string');
+   console.log(data);
    response.send(buffer);
-  });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+
+  var data = fs.readFileSync('index.html');
+  console.log(data.toString());
+
 });
